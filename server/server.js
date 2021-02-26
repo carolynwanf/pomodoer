b6d34d70321b0262a33c2b7c7fd8eabd143ffc60
Carolyn Wang <carolynwang@college.harvard.edu>
2021-02-26 12:59:42 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
-var server = app.listen(port, function() {
+const server = app.listen(port, function() {
     console.log('Node app is running on port', port);
 });
 app.use(express.static('build'));
