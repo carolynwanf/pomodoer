b722c605d16a8a9992c3b01b32364718afa19429
Carolyn Wang <carolynwang@college.harvard.edu>
2021-02-26 00:02:47 -0500

    Revert "Update server.js"
    This reverts commit 573e49355005f10fe2a54ce99a61ae0e95e47197.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server,{
-    cors: {
-        origin: "https://pomodoer.herokuapp.com/"
-    },
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 5000
+
+var server = app.listen(port, function() {
+    console.log('Node app is running on port', port);
 });
 
-const PORT = 4000;
+
+const io = require('socket.io')(server);
+
 const NEW_TASK_EVENT = "newTaskItem";
 const STATUS_CHANGE = 'statusChange';
 const CLEAR_COMPLETE = 'clearCompleteTasks';
@@ -49,6 +53,6 @@ io.on('connection', (socket) => {
     
 })
 
-server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+// server.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`)
+// })
