488223939a843a80cd55de8ab17d0dc5c45429a5
Carolyn Wang <carolynwang@college.harvard.edu>
2021-02-25 23:08:30 -0500

    AHHH

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server,{
     },
 });
 
-const PORT = 80;
+const PORT = 443;
 const NEW_TASK_EVENT = "newTaskItem";
 const STATUS_CHANGE = 'statusChange';
 const CLEAR_COMPLETE = 'clearCompleteTasks';
