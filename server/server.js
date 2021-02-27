6cc44902bff5776de3ede10f2ab72f55dfe4ee35
Carolyn Wang <carolynwang@college.harvard.edu>
2021-02-27 00:21:01 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -54,6 +54,6 @@ io.on('connection', (socket) => {
     
 })
 
-server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+// server.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`)
+// })
